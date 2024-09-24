562f5d375466ac370c1ce9e6068b64a36a36c413
Cyrill Josh Odato <151166468+CyrillJosh@users.noreply.github.com>
2024-09-24 16:36:26 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
     .then((rawData) => rawData.json())
     .then((curriculum) => {
         //console.log(curriculum);
-
+        //Initialize
+        //Semester level
         let sem = ["First Year, First Semester",
                     "First Year, Second Semester",
                     "Second Year, First Semester",
@@ -13,22 +14,76 @@ $(document).ready(function() {
                     "Fourth Year, First Semester",
                     "Fourth Year, Second Semester"];
         let semcount = 0;
+
+        const columnName = {"Course": 15,
+                            "Description": 25,
+                            "Unit": 8,
+                            "Grade": 10,
+                            "Remarks" : 12,
+                            "Course2": 16,
+                            "Term": 25}
+
+        //get the subjects for each semester
         curriculum.forEach(subjects => {
             //Header
-                console.log(sem[semcount++]);
-            for (let i = 0; i < subjects.length; i++) {
+                //Title
+                $(".tablebody").append(
+                    `<tr class="table-title">
+                        <th colspan="7">${sem[semcount++]}</th>
+                    </tr>`
+                )
+                //Column name
+                let tableHeadHTML;
+                for (let x in columnName) {
+                    //For the second course column
+                    if(x === "Course2")
+                        tableHeadHTML += `<td width=${columnName[x]}%>${x.substring(0,(x.length-1))}</td>`;  
+                    else
+                        tableHeadHTML += `<td width=${columnName[x]}%>${x}</td>`;
+                }
+                $(".tablebody").append(
+                    `<tr class="table-head">
+                        ${tableHeadHTML}
+                    </tr>`
+                )
+            //Body
+            for (let i in subjects) {
                 //Initialize data
                 let subject = subjects[i];
-                console.log(subject.Course);
-                console.log(subject.Description);
-                console.log(subject.Unit);
-                console.log(subject.Grade);
-                console.log(subject.Remarks);
+                let course = subject["Course"];
+                let desc = subject["Description"];
+                let unit = subject["Unit"];
+                let grade = subject["Grade"];
+                let remarks = subject["Remarks"];
+                let term = subject["Term"];
+
+                //Check if the subject is already taken, in progress, or not
+                let cssclass;
+                switch (remarks) {
+                    case "Passed":
+                        cssclass = "table-body-taken"
+                        break;
+                    case "":
+                        cssclass = "table-body-not-taken"
+                        break;
+                    case "In progress":
+                        cssclass = "table-body-in-progress"
+                        break;
+                }
+                
+                //Display body
+                $(".tablebody").append(
+                    `<tr class="${cssclass}">
+                    <td>${course}</td>
+                    <td>${desc}</td>
+                    <td>${unit}</td>
+                    <td>${grade}</td>
+                    <td>${remarks}</td>
+                    <td>${course}</td>
+                    <td>${term}</td>
+                    </tr>`
+                )
             }
         });
-
-        $(".tablebody").append(
-            //htmlCodes
-        )
     });
 })
